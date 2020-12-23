43063ee41b4a42011e2ebd54a3ead54674d9a22c
nikolaj <strashevskiynn@g,ail.com>
2020-12-23 18:46:39 +0300

    factorial_recursive

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -4,8 +4,9 @@ def factorial_recursive(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    return 1 if n == 0 else factorial_recursive(n-1) * n
 
 
 def factorial_iterative(n: int) -> int:
