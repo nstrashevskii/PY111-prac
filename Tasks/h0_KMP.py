05b4820294a339a7411ec8563f01512d527af921
nikolaj <strashevskiynn@g,ail.com>
2021-01-18 19:26:40 +0300

    prefix_fun

--- Tasks/h0_KMP.py
+++ Tasks/h0_KMP.py
@@ -8,8 +8,22 @@ def _prefix_fun(prefix_str: str) -> List[int]:
     :param prefix_str: dubstring for prefix function
     :return: prefix values table
     """
-    print(prefix_str)
-    return []
+    pi = [0] * len(prefix_str)  # префикс таблица
+    j = 0  # граница префикса
+    i = 1  # граница сцффикса
+
+    while i < len(prefix_str):
+        if prefix_str[i] != prefix_str[j]:
+            if j == 0:
+                pi[i] = 0
+                i += 1
+            else:
+                j = pi[j - 1]
+        else:
+            pi[i] = j + 1
+            j += 1
+            i += 1
+    return pi
 
 
 def kmp_algo(inp_string: str, substr: str) -> Optional[int]:
