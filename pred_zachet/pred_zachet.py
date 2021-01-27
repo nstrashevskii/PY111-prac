f5fdf4a3ef4deb0c9dc008847ab3c2aa75670144
nikolaj <strashevskiynn@g,ail.com>
2021-01-27 18:28:39 +0300

    начало считалки

--- pred_zachet/pred_zachet.py
+++ pred_zachet/pred_zachet.py
@@ -1,51 +1,10 @@
 import networkx as nx
 
 
-# посчитать самый дешевый путь, и вернуть то как мы шли(алгоритм дейкстры посмотреть в библиотеке nx)
-# all_way = [[2A, 7B, 9C, 3D],
-#            [12E, 4F, 1G, 9H],
-#            [1I, 5J, 2K, 5L]]
-
-
 def min_way(edge: list, node_1: str, node_2: str):
     graph = nx.Graph()
     graph.add_weighted_edges_from(edge)
     print(nx.shortest_path(graph, node_1, node_2, weight='weight'))
-
-
-#  2. поиск в глубину(ищем компонент связанности) посмотреть что такое компонент связанности
-
-
-def count_connected():
-    G = nx.Graph()
-    G.add_nodes_from("ABCDEFG")  # инициализация вершин
-    G.add_edges_from([('A', 'B'), ('B', 'C'), ('C', 'D'), ('F', 'G')])  # ребра
-    comps = nx.connected_components(G)
-    l_comps = []
-    i = 0
-    for j in comps:
-        i += 1
-        l_comps.append(j)
-        print(f'{i} комонента связности графа {j} равна {len(j)}')
-    print(f'Всего компонент связности {i}')
-    return l_comps
-
-
-# 3. Вы – компания, дающая в аренду ракеты.
-# Каждый день к вам приходит список заявок на использование ракет в виде: (час_начала, час_конца),
-# (час_начала, час_конца), ...
-# Если аренда ракеты заканчивается в час X,
-# то в этот же час ее уже можно взять в аренду снова
-# (т.е. час_начала может начинаться с Х).
-
-# Дано: список заявок на использование ракет
-# Задача: вывести ответ, хватит ли вам одной ракеты,
-# чтобы удовлетворить все заявки на этот день
-
-# решение через граф
-# граф мульти-направленный(больше чем одна связь), не можем удовлетворить заявку когда колличество
-# исходящих связей из одной вершины больше чем одна(исходящая степень(degree) вершины больше 1)
-# g1 = nx.MultiDiGraph()
 
 
 def rocket(intervals: list) -> None:
@@ -63,8 +22,31 @@ def rocket(intervals: list) -> None:
     print('Все хорошо, аренда возможна')
 
 
+def count_connected():
+    G = nx.Graph()
+    G.add_nodes_from("ABCDEFG")  # инициализация вершин
+    G.add_edges_from([('A', 'B'), ('B', 'C'), ('C', 'D'), ('F', 'G')])  # ребра
+    comps = nx.connected_components(G)
+    l_comps = []
+    i = 0
+    for j in comps:
+        i += 1
+        l_comps.append(j)
+        print(f'{i} комонента связности графа {j} равна {len(j)}')
+    print(f'Всего компонент связности {i}')
+    return l_comps
+
+
+def schitalka(n: int, k: int) -> int:
+    list_people = [i for i in range(1, n)]
+    list_slog = [j for j in range(1, k)]
+    for v in range(1, k):
+
+        if v == list_slog[k]:
+
+
 if __name__ == '__main__':
-    print(f'Задача №1 построить самый дешевый путь')
+    print(f'Задача №4 построить самый дешевый путь')
     e = [('A', 'B', 9), ('B', 'C', 9), ('C', 'D', 3), ('A', 'E', 14), ('E', 'I', 1), ('B', 'F', 4), ('F', 'J', 5),
          ('C', 'G', 1), ('G', 'K', 2), ('D', 'H', 9), ('H', 'L', 5), ('E', 'F', 4), ('F', 'G', 1), ('G', 'H', 9),
          ('I', 'J', 5), ('J', 'K', 2), ('K', 'L', 5)]
@@ -90,8 +72,10 @@ if __name__ == '__main__':
             l5.append(i[2])
     weight = min(l1) + min(l2) + min(l3) + min(l4) + min(l5)
     print(weight)
-    print(f'Задача №2 компоненты связности')
+    print('-------------')
+    print(f'Задача №3 компоненты связности')
     count_connected()
-    print(f'Задача №3 аренда ракеты')
+    print('-------------')
+    print(f'Задача №6 аренда ракеты')
     int_ = [(1, 2), (3, 4), (3, 7)]
     rocket(int_)
